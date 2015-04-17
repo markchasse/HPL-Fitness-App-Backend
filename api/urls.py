60450af4791233c8648e21561d31e8bef1435652
Muneeb Ahmad <muneebahmad222@gmail.com>
2015-04-17 12:00:01 +0500

    adding slash at the end of urls

--- api/urls.py
+++ api/urls.py
@@ -5,10 +5,10 @@ from api.views import AssignedWorkoutViewSet, SubscriptionViewSet, ResultViewSet
 
 
 urlpatterns = patterns('',
-    url(r'^workout$', AssignedWorkoutViewSet.as_view()),
+    url(r'^workout/$', AssignedWorkoutViewSet.as_view()),
 
-    url(r'^result/(?P<id>\d+)$', ResultViewSet.as_view({'get': 'get'})),
-    url(r'^result$', ResultViewSet.as_view({'post': 'create', 'put': 'update'})),
-    url(r'^subscription$', SubscriptionViewSet.as_view({'get': 'get'})),
+    url(r'^result/(?P<id>\d+)/$', ResultViewSet.as_view({'get': 'get'})),
+    url(r'^result/$', ResultViewSet.as_view({'post': 'create', 'put': 'update'})),
+    url(r'^subscription/$', SubscriptionViewSet.as_view({'get': 'get'})),
 
 )
