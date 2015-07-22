f98e4741bd7e2c17213ee6a055031e3f5c408f81
Muneeb Ahmad <muneebahmad222@gmail.com>
2015-07-22 18:35:47 +0500

    updated config to use hpl profile and csrf exempt some views

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,12 +5,12 @@ from accounts.views import Register, TokenLogin,DeleteAccount,ChangePassword,For
 
 urlpatterns = patterns('',
     url(r'^register/$', csrf_exempt(Register.as_view()), name='register_user'),
-    url(r'^login/$', TokenLogin.as_view(), name='token_login'),
+    url(r'^login/$', csrf_exempt(TokenLogin.as_view()), name='token_login'),
     url(r'^account_information$', AccountInformation.as_view(), name='account_information'),
-    url(r'^password/change/$', ChangePassword.as_view(), name='change-password'),
-    url(r'^forget_password/$', ForgetPasswordEmail.as_view()),
-    url(r'^reset_password/$', ResetPassword.as_view()),
-    url(r'^delete_account/$', DeleteAccount.as_view()),
+    url(r'^password/change/$', csrf_exempt(ChangePassword.as_view()), name='change-password'),
+    url(r'^forget_password/$', csrf_exempt(ForgetPasswordEmail.as_view())),
+    url(r'^reset_password/$', csrf_exempt(ResetPassword.as_view())),
+    url(r'^delete_account/$', csrf_exempt(DeleteAccount.as_view())),
     url(r'^parse_installation/$', ParseInstallation.as_view()),
     url(r'^apple_subscription/$', AppleSubscription.as_view()),
 
